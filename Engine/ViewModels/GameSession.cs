16ce7835befb5beecdc17531cd30e68db2b973ed
SEWEL CASTANEDA <sac4593@students.uc-bcf.edu.ph>
2024-03-11 15:36:59 +0800

    Implemented a button that increases the player xp by 10

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -9,7 +9,7 @@ namespace Engine.ViewModels
 {
     public class GameSession
     {
-        Player CurrentPlayer { get; set; }
+        public Player CurrentPlayer { get; set; }
         public GameSession()
         {
             CurrentPlayer = new Player();
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,4 +24,8 @@ public partial class MainWindow : Window
         _gameSession = new GameSession();
         DataContext = _gameSession;
     }
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        _gameSession.CurrentPlayer.ExperiencePoints += 10;
+    }
 }
