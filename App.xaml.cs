66830d587e080ceaa4d8eff959d6f5b6d5a1cc39
MY PC <MY PC@ADDENDUM>
2024-03-11 11:50:36 +0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CC3_1N_GYMRAT;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
