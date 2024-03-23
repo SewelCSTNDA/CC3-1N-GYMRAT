a11a1e925f7ba1aefaf8c1ab22cec2954a1a1da3
SEWEL CASTANEDA <sac4593@students.uc-bcf.edu.ph>
2024-03-23 18:59:58 +0800

    Implemented Weapons and Game Item classes and factories

---
+++ Engine/Factories/ItemFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    public static class ItemFactory
+    {
+        private static List<GameItem> _standardGameItems;
+
+        static ItemFactory()
+        {
+            _standardGameItems = new List<GameItem>();
+
+            _standardGameItems.Add(new Weapon(001, "Blunt Sword", 10, 4, 6));
+            _standardGameItems.Add(new Weapon(002, "Iron Sword", 50, 10, 12));
+        }
+
+        public static GameItem CreateGameItem(int itemTypeId)
+        {
+            GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeId == itemTypeId);
+
+            if(standardItem != null)
+            {
+                return standardItem.Clone();
+            }
+
+            return null;
+        }
+    }
+}
